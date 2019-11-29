b84055f9ac28130aab638c5883206d54129d5824
Tilman Baumann <tilman.baumann@canonical.com>
2019-11-29 12:54:38 +0100

    Missing files

---
+++ lib/bgp_advertiser/util.py
@@ -0,0 +1,10 @@
+
+def get_neighbours():
+    # FIXME spaces
+    neighbours = [
+        {
+            neighbour: "1.2.3.1",
+            local-address: "1.2.3.2"
+        }
+    ]
+    return neighbours
