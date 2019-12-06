08537f777e05cd7622c78b692ce98b91581f3514
Tilman Baumann <tilman.baumann@canonical.com>
2019-12-06 16:38:00 +0100

    Mostly working now.
    Still rough around the edges.
    Exabgp doesn't work, it runs as user nobody which is not allowed to add
    the interfaces.

--- lib/bgp_advertiser/util.py
+++ lib/bgp_advertiser/util.py
@@ -1,10 +1,37 @@
+from pyroute2 import IPRoute
+from charmhelpers.core.hookenv import log
+from charms.layer import status
+from charms.reactive import set_flag, clear_flag
+REQUIRED_FIELDS = ['vips', 'local_as', 'peer_as', 'router_id', 'neighbors']
 
-def get_neighbours():
+def get_neighbours(config):
     # FIXME spaces
-    neighbours = [
-        {
-            'neighbour': "1.2.3.1",
-            'local_address': "1.2.3.2"
-        }
-    ]
+    neighbours = []
+    with IPRoute() as ipr:
+        for neighbour in config['neighbors'].split(','):
+            try: # Catch illegal IP and stuff like that
+                routes = ipr.route('get', dst=neighbour)
+                for route in routes: #Just for safety in case respose is empty or multiple elements
+                    attrs = dict(route['attrs'])
+                    neighbours.append({
+                        'neighbour': neighbour,
+                        'local_address': attrs['RTA_PREFSRC']
+                    })
+            except:
+                log("Skipping bad neighbor entry {}".format(neighbour))
+                next
     return neighbours
+
+def has_required_fields(config):
+    missing_fields = []
+    for field in REQUIRED_FIELDS:
+        if not config[field]:
+            missing_fields.append(field)
+    if len(missing_fields) > 0:
+        log("Blocked missing options [{}]".format(" ".join(missing_fields)))
+        status.blocked("Configuration incomplete. Required fields {}".format(" ".join(missing_fields)))
+        set_flag('bgp-advertiser.blocked.options')
+        return False
+    else:
+        clear_flag('bgp-advertiser.blocked.options')
+        return True
--- reactive/bgp_advertiser.py
+++ reactive/bgp_advertiser.py
@@ -1,49 +1,75 @@
 from charms.reactive import when, when_not, set_flag, hook, clear_flag
 from charmhelpers.core.hookenv import log, config, service_name
+from charmhelpers.core.host import service_start, service_stop, service_restart, service_pause, service_resume
 from charms.templating.jinja2 import render
 from charms.layer import status
 import charms.coordinator
 
-from bgp_advertiser.util import get_neighbours
+from bgp_advertiser.util import get_neighbours, has_required_fields
 
 EXABGP_CONF = '/etc/exabgp/exabgp.conf'
-
+SERVICENAME = 'exabgp'
 
 @when_not('bgp-advertiser.ready')
 @when('apt.installed.exabgp')
 def install_bgp_advertiser():
-    # FIXME register service
+    # FIXME Do we even need this post-inst step?
     set_flag('bgp-advertiser.ready')
+    write_config()
 
 
 @when('bgp-advertiser.ready')
+@when('bgp-advertiser.running')
 def is_ready():
-    # FIXME dislay active node count
     status.active('Ready')
+
+
+@when('bgp-advertiser.should-run')
+@when_not('bgp-advertiser.running')
+def start_service():
+    log("Starting exabgp service")
+    service_resume(SERVICENAME)
+    service_start(SERVICENAME)
+    set_flag('bgp-advertiser.running')
+
+
+@when_not('bgp-advertiser.should-run')
+@when('bgp-advertiser.running')
+def stop_service():
+    log("Stopping exabgp service")
+    service_pause(SERVICENAME)
+    service_stop(SERVICENAME)
+    clear_flag('bgp-advertiser.running')
 
 
 @when('bgp-advertiser.ready')
 @when('config.changed')
 def write_config():
     context = config()
-    context['neighbours'] = get_neighbours()
-    context['app_name'] = service_name()
-    render('exabgp.conf', EXABGP_CONF, context)
-    charms.coordinator.acquire('restart')
+    if has_required_fields(context):
+        context['neighbours'] = get_neighbours(context)
+        context['app_name'] = service_name()
+        log("VIP={}".format(context['vips']))
+        render('exabgp.conf', EXABGP_CONF, context)
+        set_flag('bgp-advertiser.should-run')
+        charms.coordinator.acquire('restart')
+    else:
+        clear_flag('bgp-advertiser.should-run')
 
 
 @when('coordinator.granted.restart')
 def restart():
     status.maintenance('Rolling restart')
-    # FIXME restart()
+    service_restart(SERVICENAME)
     status.active('Live')
 
 
 @hook('stop')
 def stopped():
-    #FIXME Cleanup and disable service
     log("Uninstalling")
+    clear_flag('bgp-advertiser.should-run')
 
 @hook('upgrade-charm')
 def upgrade_charm():
     write_config()
+
